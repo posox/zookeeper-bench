3584672dd3804da5bcd070edc086414bcf6c5a75
Sergey Reshetnyak <sreshetniak@mirantis.com>
2015-11-27 16:51:03 +0300

    Add retry support

--- test.py
+++ test.py
@@ -7,13 +7,12 @@ import logging
 import os
 import random
 import string
+import time
 
 from kazoo import client
 
 
 logging.basicConfig()
-
-
 LOG = logging.getLogger(__name__)
 
 
@@ -42,17 +41,20 @@ def main():
         pid = os.fork()
         if pid == 0:
             print("Start child process %d" % os.getpid())
+            run_bench(args.servers, args.message_count, args.znode_size,
+                      args.watchers)
             break
         else:
             children.append(pid)
 
-    if pid == 0:
-        run_bench(args.servers, args.message_count, args.znode_size,
-                  args.watchers)
-    else:
+    if pid > 0:
+        failed = 0
         for child in children:
             pid, exit_code = os.waitpid(child, 0)
             print("Child process %d exited with code %d" % (pid, exit_code))
+            if exit_code != 0:
+                failed += 1
+        print("Total failed tasks: %d" % failed)
 
 
 def watcher(event):
@@ -64,42 +66,45 @@ def get_znode_path(path, number):
 
 
 def run_bench(zk_hosts, message_count, znode_size, watchers):
-    zk = client.KazooClient(hosts=",".join(zk_hosts))
-    zk.start()
+    zk = client.KazooClient(hosts=",".join(zk_hosts), timeout=150)
+    start_sleep = random.randrange(0, 1200, 1) / 10.0
+    time.sleep(start_sleep)
+    zk.retry(zk.start)
+    time.sleep(120 - start_sleep)
     path = "/my/path-%s" % get_random_str(8)
-    zk.ensure_path(path)
+    zk.retry(zk.ensure_path, path)
 
     # create znode
     for x in range(message_count):
         data_path = get_znode_path(path, x)
-        zk.create(data_path, str(x) * znode_size)
+        zk.retry(zk.create, data_path, str(x) * znode_size)
 
     # get znode
     for x in range(message_count):
         data_path = get_znode_path(path, x)
-        zk.get(data_path)
+        zk.retry(zk.get, data_path)
 
     # update znode
     for x in range(message_count):
         data_path = get_znode_path(path, x)
-        zk.set(data_path, str(x + 1))
+        zk.retry(zk.set, data_path, str(x + 1))
 
     # create watchers
     if watchers > 0:
         for x in range(message_count):
             data_path = get_znode_path(path, x)
             for w in range(watchers):
-                zk.get_children(data_path, watch=watcher)
+                zk.retry(zk.get_children, data_path, watch=watcher)
 
     # call watchers
     if watchers > 0:
         for x in range(message_count):
             data_path = get_znode_path(path, x)
-            zk.set(data_path, str(x + 2))
+            zk.retry(zk.set, data_path, str(x + 2))
 
     # delete znodes
-    zk.delete(path, recursive=True)
-    zk.stop()
+    zk.retry(zk.delete, path, recursive=True)
+    zk.retry(zk.stop)
 
 
 if __name__ == "__main__":
